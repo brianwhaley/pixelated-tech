87752f7d370f2258348ede2c0c6cf22c9e34a1c5
Brian Whaley <brian.whaley@gmail.com>
2026-07-10 03:15:37 -0400

    chore: release 1.0.15

--- apps/brianwhaley/src/app/elements/nav.tsx
+++ apps/brianwhaley/src/app/elements/nav.tsx
@@ -2,15 +2,13 @@
 
 import React from "react";
 import { MenuAccordion, usePixelatedConfig } from "@pixelated-tech/components";
-// import { getAccordionMenuData } from "../components/metadata/pixelated.metadata";
 
 export default function Nav() {
 	const pixelatedConfig = usePixelatedConfig();
-	const routes = pixelatedConfig?.routes ?? [];
+	const routes = pixelatedConfig?.routes as any[] ?? [];
 
 	// const menuItems = getAccordionMenuData(allRoutes);
 	return (
 		<MenuAccordion menuItems={routes} />
 	);
 }
-	
